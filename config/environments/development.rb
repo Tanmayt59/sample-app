0d4e519188c2994fe983fe4d7ef6c4b4577943e5
Tanmayt59 <tanmaytiwari59@gmail.com>
2020-09-17 22:39:26 +0530

    Add account activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,6 +58,31 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
+  
+  
+  config.action_mailer.raise_delivery_errors = false
+host = 'localhost:3000' # Don't use this literally; use your local dev host instead
+# Use this on the cloud IDE.
+  
+  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+# Use this if developing on localhost.
+# config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   # Allow connections to local server.
  config.hosts.clear
