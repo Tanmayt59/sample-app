b492f551df3a82760de0a53c6780f8d30cae36d5
Tanmayt59 <tanmaytiwari59@gmail.com>
2020-09-21 16:56:33 +0530

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,14 @@ get '/signup', to: 'users#new'
 get '/login',     to:     'sessions#new'
 post '/login',    to:    'sessions#create'
 delete '/logout', to: 'sessions#destroy'
-   resources :users
+
+resources :users do
+member do
+get :following, :followers
+end
+end
    resources :account_activations, only: [:edit]
    resources :microposts, only: [:create, :destroy]
+   resources :relationships, only: [:create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
