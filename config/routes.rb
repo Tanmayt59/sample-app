13aa603e12096270b8e01cafb444d37913524943
Tanmayt59 <tanmaytiwari59@gmail.com>
2020-10-03 16:12:25 +0530

    Formatted

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,23 @@
 Rails.application.routes.draw do
 
-  get 'sessions/new'
-root 'static_pages#home'
+	get 'sessions/new'
+	root 'static_pages#home'
 
- get '/help', to: 'static_pages#help'
-get '/about', to: 'static_pages#about'
-get '/contact', to: 'static_pages#contact'
-get '/signup', to: 'users#new'
-get '/login',     to:     'sessions#new'
-post '/login',    to:    'sessions#create'
-delete '/logout', to: 'sessions#destroy'
+	get '/help', to: 'static_pages#help'
+	get '/about', to: 'static_pages#about'
+	get '/contact', to: 'static_pages#contact'
+	get '/signup', to: 'users#new'
+	get '/login',     to:     'sessions#new'
+	post '/login',    to:    'sessions#create'
+	delete '/logout', to: 'sessions#destroy'
 
-resources :users do
-member do
-get :following, :followers
-end
-end
-   resources :account_activations, only: [:edit]
-   resources :microposts, only: [:create, :destroy]
-   resources :relationships, only: [:create, :destroy]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+	resources :users do
+		member do
+			get :following, :followers
+		end
+	end
+	resources :account_activations, only: [:edit]
+	resources :microposts, only: [:create, :destroy]
+	resources :relationships, only: [:create, :destroy]
+	# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,18 @@
 # Create a main sample user.
- User.create!(name: "Example",
-email: "example1@railstutorial.org",
+User.create!(name: "Example",
+	email: "example1@railstutorial.org",
 password: "foobar", password_confirmation: "foobar", admin: true)
 # Generate a bunch of additional users.
 99.times do |n|
-name = Faker::Name.name
-email = "example-#{n+1}@railstutorial.org"
-password = "password"
- User.create!(name: name, email: email, password: password, password_confirmation: password)
+	name = Faker::Name.name
+	email = "example-#{n+1}@railstutorial.org"
+	password = "password"
+	User.create!(name: name, email: email, password: password, password_confirmation: password)
 end
 users = User.order(:created_at).take(6)
 50.times do
-content = Faker::Lorem.sentence(word_count: 5)
-users.each { |user| user.microposts.create!(content: content) }
+	content = Faker::Lorem.sentence(word_count: 5)
+	users.each { |user| user.microposts.create!(content: content) }
 end
 # Create following relationships.
 users = User.all
